4899d8c2bbd25b6d90f16cbf95ba24bec80dca25
zzcoding530 <rookiezc@163.com>
2021-01-07 12:55:08 +0800

    智力题啊。。。我服了

---
+++ src/easy/Solution292.java
@@ -0,0 +1,13 @@
+package easy;
+
+/**
+ * @author: zhangchen
+ * @date: 2021/1/7
+ * @description: nim游戏
+ */
+
+public class Solution292 {
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}
