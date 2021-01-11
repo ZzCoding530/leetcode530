46f06fc192c89b0389ac191d262ef5fdf23a9741
zzcoding530 <rookiezc@163.com>
2021-01-11 12:30:47 +0800

    改了一下解答

--- src/easy/Solution20.java
+++ src/easy/Solution20.java
@@ -1,6 +1,9 @@
 package easy;
 
-import java.util.Stack;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * @author: zhangchen
@@ -10,21 +13,35 @@ import java.util.Stack;
 
 class Solution20 {
     public boolean isValid(String s) {
-        if (s.isEmpty()) {
-            return true;
+
+        //奇数的必不对
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
         }
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (stack.empty() || c != stack.pop()) {
-                return false;
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+
+                //      栈空了      或者  栈顶元素和下一个元素括号不匹配
+                if (stack.isEmpty() || !stack.peek().equals(pairs.get(ch))) {
+                    return false;
+                }
+
+                //能匹配上那就把栈顶元素出栈
+                stack.pop();
+            } else {
+                stack.push(ch);
             }
         }
-        return stack.empty();
+        return stack.isEmpty();
     }
 }
+
