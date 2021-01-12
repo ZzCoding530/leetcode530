1c14591ebe1ad831359cb56be313ce022e07c3ef
zzcoding530 <rookiezc@163.com>
2021-01-12 11:09:37 +0800

    日常更新

---
+++ src/easy/Solution234.java
@@ -0,0 +1,41 @@
+package easy;
+
+
+/**
+ * @author: zhangchen
+ * @date: 2021/1/12
+ * @description:
+ */
+
+public class Solution234 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow, fast;
+        slow = fast = head;
+        ListNode pre = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode temp = slow.next;
+            slow.next = pre;
+            pre = slow;
+            slow = temp;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) {
+                return false;
+            }
+            pre = pre.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+}
