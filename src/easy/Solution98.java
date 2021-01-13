c5f0c4066982d0a973a9c3ae2ded9e4bbcc2ac63
zzcoding530 <rookiezc@163.com>
2021-01-13 14:06:07 +0800

    日常更新

---
+++ src/easy/Solution98.java
@@ -0,0 +1,28 @@
+package easy;
+
+/**
+ * @author: zhangchen
+ * @date: 2021/1/13
+ * @description:
+ */
+class Solution98 {
+    long pre = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 访问左子树
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        // 访问右子树
+        return isValidBST(root.right);
+    }
+}
+
