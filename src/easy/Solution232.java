6de8a987f8369d667ea902a28b963aea24ebc1c0
zzcoding530 <rookiezc@163.com>
2021-01-17 15:25:17 +0800

    日常更新

---
+++ src/easy/Solution232.java
@@ -0,0 +1,56 @@
+package easy;
+
+import java.util.Stack;
+
+/**
+ * @author: zhangchen
+ * @date: 2021/1/17
+ * @description:
+ */
+
+class Solution232 {
+
+    private Stack<Integer> stack1, stack2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Solution232() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        peek();
+        return stack2.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
