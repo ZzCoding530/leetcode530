4d65cc35b3c3e710be09cfdb3825cdbfc862db0f
zzcoding530 <rookiezc@163.com>
2021-02-07 17:01:09 +0800

    日常更新0.0

---
+++ src/offer/Solution3201.java
@@ -0,0 +1,39 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: zhangchen
+ * @date: 2021/2/7
+ * @description: 从上到下打印二叉树
+ */
+
+public class Solution3201 {
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>() {{
+            add(root);
+        }};
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+
+}
