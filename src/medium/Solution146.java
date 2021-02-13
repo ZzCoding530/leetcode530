ce1e5524e1265ecb2a0e9680e970de115ae08e31
zzcoding530 <rookiezc@163.com>
2021-02-13 14:50:21 +0800

    日常更新0.0

--- src/medium/Solution146.java
+++ src/medium/Solution146.java
@@ -1,6 +1,6 @@
 package medium;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 
 /**
  * @author: zhangchen
@@ -9,39 +9,111 @@ import java.util.LinkedHashMap;
  */
 
 public class Solution146 {
-    int cap;
-    LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+
+
+    private int capacity;
+    private HashMap<Integer, ListNode> hashmap;
+    private ListNode head;
+    private ListNode tail;
+
+    /**
+     * 内建节点类，双向链表节点
+     */
+    private class ListNode {
+        int key;
+        int val;
+        ListNode prev;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
 
     public Solution146(int capacity) {
-        this.cap = capacity;
+        this.capacity = capacity;
+        hashmap = new HashMap<>();
+        head = new ListNode();
+        tail = new ListNode();
+        head.next = tail;
+        tail.prev = head;
     }
 
+    /**
+     * 删除某节点    把节点自身架空
+     *
+     * @param node 需要被删除的节点
+     */
+    private void removeNode(ListNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    /**
+     * 在最后添加节点
+     *
+     * @param node 要添加的节点
+     */
+    private void addNodeToLast(ListNode node) {
+        node.prev = tail.prev;
+        node.prev.next = node;
+        node.next = tail;
+        tail.prev = node;
+    }
+
+    /**
+     * 移动节点到末尾  1.删除节点  2.节点添加到末尾
+     *
+     * @param node
+     */
+    private void moveNodeToLast(ListNode node) {
+        removeNode(node);
+        addNodeToLast(node);
+    }
+
+    /**
+     * 通过key取得节点，最新用的放到末尾
+     *
+     * @param key index
+     * @return 节点的值
+     */
     public int get(int key) {
-        if (!cache.containsKey(key)) {
+        if (hashmap.containsKey(key)) {
+            ListNode node = hashmap.get(key);
+            moveNodeToLast(node);
+            return node.val;
+        } else {
             return -1;
         }
-        makeRecent(key);
-        return cache.get(key);
     }
 
+    /**
+     * 要把操作过的节点都放在末尾
+     * 容量满了要删除最没被用过的 放在首位的
+     *
+     * @param key   key
+     * @param value value
+     */
     public void put(int key, int value) {
-        if (cache.containsKey(key)) {
-            cache.put(key, value);
-            makeRecent(key);
+        if (hashmap.containsKey(key)) {
+            ListNode node = hashmap.get(key);
+            node.val = value;
+            moveNodeToLast(node);
             return;
         }
-
-        if (cache.size() >= this.cap) {
-            int oldestKey = cache.keySet().iterator().next();
-            cache.remove(oldestKey);
+        if (hashmap.size() == capacity) {
+            hashmap.remove(head.next.key);
+            removeNode(head.next);
         }
 
-        cache.put(key, value);
+        ListNode node = new ListNode(key, value);
+        hashmap.put(key, node);
+        addNodeToLast(node);
     }
 
-    public void makeRecent(int key) {
-        int val = cache.get(key);
-        cache.remove(key);
-        cache.put(key, val);
-    }
+
 }
