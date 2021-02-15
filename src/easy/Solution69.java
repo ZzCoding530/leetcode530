057b7225895a7c323bbef1d48549febe1c89d791
zzcoding530 <rookiezc@163.com>
2021-02-15 20:34:10 +0800

    日常更新

--- src/easy/Solution69.java
+++ src/easy/Solution69.java
@@ -9,27 +9,19 @@ package easy;
 public class Solution69 {
 
     public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
-        // 注意：针对特殊测试用例，例如 2147395599
-        // 要把搜索的范围设置成长整型
-        long left = 1;
-        long right = x / 2;
-        while (left < right) {
-            // 注意：这里一定取右中位数，如果取左中位数，代码会进入死循环
-            // long mid = left + (right - left + 1) / 2;
-            long mid = (left + right + 1) >>> 1;
-            long square = mid * mid;
-            if (square > x) {
-                right = mid - 1;
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
             } else {
-                left = mid;
+                r = mid - 1;
             }
         }
-        // 因为一定存在，因此无需后处理
-        return (int) left;
+        return ans;
     }
+
 
 }
 
