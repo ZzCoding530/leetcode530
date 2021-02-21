81d3f72416439120e03cb25b2581d7ec7241e09b
zzcoding530 <rookiezc@163.com>
2021-02-21 18:22:33 +0800

    合并两个有序链表 递归一看就会一写就废 哭了

---
+++ src/top100/Solution5.java
@@ -0,0 +1,38 @@
+package top100;
+
+/**
+ * @author: zhangchen
+ * @date: 2021/2/21
+ * @description: 合并两个有序链表
+ */
+
+public class Solution5 {
+    /**
+     * !!!!!!求求一定要先处理特殊情况!!!!!!
+     * <p>
+     * 递归啊啊啊啊啊啊啊啊啊
+     * 递归啊啊啊啊啊啊
+     * 递归啊啊啊
+     * 递归啊
+     *
+     * @param l1 链表1
+     * @param l2 链表2
+     * @return 结果链表
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
