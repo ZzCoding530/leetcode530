17014ee5d1df0e7bd0e25a98542c73f45c08776a
zzcoding530 <rookiezc@163.com>
2021-02-27 17:20:23 +0800

    前序和中序重构出二叉树 这个思路还行，coding有难度

--- src/top100/Solution15.java
+++ src/top100/Solution15.java
@@ -9,41 +9,48 @@ import java.util.HashMap;
  */
 
 public class Solution15 {
-    private HashMap<Integer, Integer> indexMap;
+    private HashMap<Integer, Integer> map;
 
-    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
-        if (preorder_left > preorder_right) {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int len = preorder.length;
+        map = new HashMap<>(len);
+
+        for (int i = 0; i < len; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return buildTree(preorder, inorder, 0, len - 1, 0, len - 1);
+    }
+
+    /**
+     * 思路不难，coding确实挺难
+     *
+     * @param preorder                前序遍历得到的数组
+     * @param inorder                 中序遍历得到的数组
+     * @param left_bound_of_preorder  用来划分前序数组的左边界
+     * @param right_bound_of_preorder 用来划分前序数组的右边界
+     * @param left_bound_of_inorder   用来划分中序数组的左边界
+     * @param right_bound_of_inorder  用来划分中序数组的右边界
+     * @return 返回重构出的二叉树
+     */
+    private TreeNode buildTree(int[] preorder, int[] inorder, int left_bound_of_preorder, int right_bound_of_preorder, int left_bound_of_inorder, int right_bound_of_inorder) {
+
+        if (left_bound_of_preorder > right_bound_of_preorder) {
             return null;
         }
 
-        // 前序遍历中的第一个节点就是根节点
-        int preorder_root = preorder_left;
-        // 在中序遍历中定位根节点
-        int inorder_root = indexMap.get(preorder[preorder_root]);
+        int root_index_in_pre = left_bound_of_preorder;
+        int root_index_in_in = map.get(preorder[root_index_in_pre]);
 
-        // 先把根节点建立出来
-        TreeNode root = new TreeNode(preorder[preorder_root]);
-        // 得到左子树中的节点数目
-        int size_left_subtree = inorder_root - inorder_left;
-        // 递归地构造左子树，并连接到根节点
-        //                      前序遍历 preorder = [3,9,20,15,7]
-        //                      中序遍历 inorder  = [9,3,15,20,7]
-        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
-        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
-        // 递归地构造右子树，并连接到根节点
-        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
-        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        TreeNode root = new TreeNode(preorder[root_index_in_pre]);
+
+        int size_of_left_subtree = root_index_in_in - left_bound_of_inorder;
+
+        root.left = buildTree(preorder, inorder, left_bound_of_preorder + 1, left_bound_of_preorder + size_of_left_subtree, left_bound_of_inorder, root_index_in_in);
+        root.right = buildTree(preorder, inorder, left_bound_of_preorder + size_of_left_subtree + 1, right_bound_of_preorder, root_index_in_in + 1, right_bound_of_inorder);
         return root;
-    }
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        // 构造哈希映射，帮助我们快速定位根节点
-        indexMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < n; i++) {
-            indexMap.put(inorder[i], i);
-        }
-        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+
     }
 
 
