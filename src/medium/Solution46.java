5681867d156e97b1a47109f02a7be040ba996c0d
zzcoding530 <rookiezc@163.com>
2021-04-11 16:56:33 +0800

    两个全排列  回溯法

---
+++ src/medium/Solution46.java
@@ -0,0 +1,52 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: zhangchen
+ * @date: 2021/4/11
+ * @description: 全排列
+ */
+
+public class Solution46 {
+    public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+
+        List<List<Integer>> resList = new LinkedList<>();
+        if (len == 0) {
+            return resList;
+        }
+
+        boolean[] used = new boolean[len];
+
+        List<Integer> eachList = new LinkedList<>();
+        dfs(len, 0, used, nums, eachList, resList);
+
+        return resList;
+    }
+
+
+    private void dfs(int len, int depth, boolean[] used, int[] nums, List<Integer> eachList, List<List<Integer>> resList) {
+        if (len == depth) {
+            resList.add(new ArrayList<>(eachList));
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                eachList.add(nums[i]);
+                used[i] = true;
+
+
+                dfs(len, depth + 1, used, nums, eachList, resList);
+
+                used[i] = false;
+                eachList.remove(eachList.size() - 1);
+            }
+        }
+
+    }
+
+}
