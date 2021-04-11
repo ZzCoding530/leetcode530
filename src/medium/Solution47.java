cf26c943d489c996a2f8fac513a93ef6266f4e1f
zzcoding530 <rookiezc@163.com>
2021-04-11 17:00:16 +0800

    不重复的全排列

--- src/medium/Solution47.java
+++ src/medium/Solution47.java
@@ -1,8 +1,6 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author: zhangchen
@@ -14,45 +12,47 @@ public class Solution47 {
     public List<List<Integer>> permuteUnique(int[] nums) {
         int len = nums.length;
 
-        List<List<Integer>> resList = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
         if (len == 0) {
-            return resList;
+            return res;
         }
+
+        // 排序（升序或者降序都可以），排序是剪枝的前提
+        Arrays.sort(nums);
 
         boolean[] used = new boolean[len];
 
-        List<Integer> eachList = new LinkedList<>();
-        dfs(len, 0, used, nums, eachList, resList);
-
-        return resList;
+        // 使用 Deque 是 Java 官方 Stack 类的建议
+        Deque<Integer> path = new ArrayDeque<>(len);
+        dfs(nums, len, 0, used, path, res);
+        return res;
     }
 
-
-    private void dfs(int len, int depth, boolean[] used, int[] nums, List<Integer> eachList, List<List<Integer>> resList) {
-        if (len == depth) {
-            resList.add(new ArrayList<>(eachList));
+    private void dfs(int[] nums, int len, int depth, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(path));
             return;
         }
 
-        for (int i = 0; i < len; i++) {
-
+        for (int i = 0; i < len; ++i) {
             if (used[i]) {
                 continue;
             }
 
+            // 剪枝条件：i > 0 是为了保证 nums[i - 1] 有意义
+            // 写 !used[i - 1] 是因为 nums[i - 1] 在深度优先遍历的过程中刚刚被撤销选择
             if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
                 continue;
             }
 
-            eachList.add(nums[i]);
+            path.addLast(nums[i]);
             used[i] = true;
 
-
-            dfs(len, depth + 1, used, nums, eachList, resList);
-
+            dfs(nums, len, depth + 1, used, path, res);
+            // 回溯部分的代码，和 dfs 之前的代码是对称的
             used[i] = false;
-            eachList.remove(eachList.size() - 1);
+            path.removeLast();
         }
-
     }
+
 }
