4d8c3ea6634b2e8604ee6d5e58bd1e595b4b9197
zzcoding530 <rookiezc@163.com>
2021-04-16 09:20:42 +0800

    翻转二叉树 带注释

--- src/easy/Solution226.java
+++ src/easy/Solution226.java
@@ -9,15 +9,19 @@ package easy;
 
 public class Solution226 {
     public TreeNode invertTree(TreeNode root) {
+
+        //节点是空的就不用翻转了
         if (root == null) {
             return null;
         }
 
+        //左右节点交换，类似链表反转
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
 
 
+        //然后递归地处理这个节点的左节点和右节点
         invertTree(root.left);
         invertTree(root.right);
 
